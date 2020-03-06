e0aa91139aa4b289f3b161005434578de9d953ba
KamilLatipov <latipovkamil@mail.ru>
2020-03-06 22:07:51 +0300

    release 1.9.1

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,15 @@
 <template>
-    <div>
         <header class="main-header">
            <nav class="main-nav">
             <div class="main-nav__link main-nav__image">
                 <img width="67" height="67" src="../assets/house.png"/>
             </div>
              <div v-if="isLoggedIn" class="main-nav__user">
-                  <p>Приветствуем на сайте rent-abrom.ru! </p>
+                  <p class="main-nav__welcome">Приветствуем на сайте rent-abrom.ru! </p>
                   <a class="main-nav__logout" @click="logout">Выйти</a>
              </div>
            </nav>
         </header>
-    </div>
 </template>
 
 <script>
